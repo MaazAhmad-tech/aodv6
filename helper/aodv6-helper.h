4569276732b65bf2fd22ad3c972981deef1e9dca
Maaz Ahmad <maaz.ahmad1912@gmail.com>
2021-03-16 15:09:55 +0530

    First Commit

---
+++ helper/aodv6-helper.h
@@ -0,0 +1,14 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+#ifndef AODV6_HELPER_H
+#define AODV6_HELPER_H
+
+#include "ns3/aodv6.h"
+
+namespace ns3 {
+
+/* ... */
+
+}
+
+#endif /* AODV6_HELPER_H */
+
---
+++ model/aodv6.h
@@ -0,0 +1,12 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+#ifndef AODV6_H
+#define AODV6_H
+
+namespace ns3 {
+
+/* ... */
+
+}
+
+#endif /* AODV6_H */
+
